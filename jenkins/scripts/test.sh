ebd7f9a980cf8831b036273e7d9d4be1f318a05e
root <root@ip-10-0-54-169.us-west-2.compute.internal>
2018-09-11 07:24:53 +0000

    Add 'Deliver…​' and 'Deploy…​' stages

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -17,4 +17,7 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
+ifconfig
+hostname
+whereis mocha
 mocha test/test.js
