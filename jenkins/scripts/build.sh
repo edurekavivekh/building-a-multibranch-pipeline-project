a29e4a05727c195aa62f2c706a7119f935bdad20
root <root@ip-10-0-54-169.us-west-2.compute.internal>
2018-09-18 08:14:33 +0000

    Add for 'Deliver for Development'

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 BUILD="$(($BUILD_ID-2))"
-docker -H 127.0.0.1 rmi --force chatapp:$BUILD
-docker -H 127.0.0.1 run -d -p 80:3000 --name chatapp chatapp:env.$BUILD_ID
+docker -H 10.0.54.169 rmi --force chatapp:$BUILD
+docker -H 10.0.54.169 run -d -p 80:3000 --name chatapp chatapp:env.$BUILD_ID
 
