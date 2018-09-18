28b9957067813896bcd31c6d1e4c37137fe548dc
root <root@ip-10-0-54-169.us-west-2.compute.internal>
2018-09-18 08:19:30 +0000

    Add for 'Deliver for Development'

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -x
 
+docker -H 10.0.54.169 stop chatapp
 #BUILD="$(($BUILD_ID-2))"
 #docker -H 10.0.54.169 rmi --force chatapp:$BUILD
-docker -H 10.0.54.169 run -d -p 80:3000 --name chatapp chatapp:env.$BUILD_ID
-
+         if [ "$?" == "0" ]
+             then
+                 docker -H 10.0.54.169 build . -t chatapp:$BUILD_ID
+             if [ "$?" == "0" ]
+                 then
+                     docker -H 10.0.54.169 rm chatapp
+                     docker -H 10.0.54.169 run -d -p 80:3000 --name chatapp chatapp:$BUILD_ID
+                 else
+                     echo "Chat app build failed"
+             fi
+         fi
